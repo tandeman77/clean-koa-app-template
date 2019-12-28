5e27eec2dc1c08e18e04cb4a3abad64d0a543b3b
tandeman <thawin999@gmail.com>
2019-12-28 14:44:10 +1000

    added routes and body

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const env = require('dotenv').config();
 const Koa = require('koa');
 const body = require('koa-body');
+const Router = require('koa-router');
+const router = new Router();
 
 const app = new Koa();
+app.use(body());
 
+//routes
+router.get('/', (ctx) => {
+  ctx.body = 'Hello World';
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+//initialise the app.
 app.listen(process.env.PORT, () => {
   console.log('app is working on port ' + process.env.PORT);
 });
